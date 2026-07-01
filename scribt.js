726cdbd65e3cffe87e61dcc04d6f86cf09ab74a7
paulitos-place <isidorigel2010@yahoo.com>
2026-07-01 17:56:15 +0200

    Add files via upload

--- scribt.js
+++ scribt.js
@@ -1,115 +1,15 @@
-import { auth } from "./firebase.js";
-
-import {
-createUserWithEmailAndPassword,
-signInWithEmailAndPassword
-}
-from
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-
-
-alert("script läuft");
-
-
-
-const username = document.getElementById("username");
-const password = document.getElementById("password");
-
+console.log("SCRIPT GELADEN");
 
 
 document.getElementById("register").onclick = function(){
 
-
-let user = username.value;
-let pass = password.value;
-
-
-console.log("Register gedrückt");
-
-
-createUserWithEmailAndPassword(
-auth,
-user + "@imaginace.fake",
-pass
-)
-
-.then(()=>{
-
-console.log("Account erstellt");
-
-showWebsite();
-
-})
-
-.catch((error)=>{
-
-alert(error.message);
-
-});
-
+alert("REGISTER GEHT");
 
 };
-
-
-
-
 
 
 document.getElementById("login").onclick = function(){
 
-
-let user = username.value;
-let pass = password.value;
-
-
-console.log("Login gedrückt");
-
-
-signInWithEmailAndPassword(
-
-auth,
-
-user + "@imaginace.fake",
-
-pass
-
-)
-
-.then(()=>{
-
-
-console.log("Login erfolgreich");
-
-showWebsite();
-
-
-})
-
-
-.catch((error)=>{
-
-
-alert("Login Fehler: " + error.message);
-
-
-});
-
+alert("LOGIN GEHT");
 
 };
-
-
-
-
-
-
-function showWebsite(){
-
-
-document.getElementById("loginScreen").style.display="none";
-
-
-document.getElementById("website").style.display="block";
-
-
-}
